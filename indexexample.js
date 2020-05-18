20ef0eadee3fa6f24efae2d21bbc822699922053
Yashika Bihani <yashikabihani7@gmail.com>
2020-05-19 01:51:54 +0530

    SetState React

---
+++ indexexample.js
@@ -0,0 +1,33 @@
+//Functional Components
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+// function MyInfo(){
+//   return (
+//     <div>
+//       <h1>Yashika Bihani</h1>
+//       <p>I am a software developer at Cognizant. Have been working for about 3 years in developing websites. I am enthusiast in Adventure Sports.</p>
+//       <ol>
+//         <li>Greece</li>
+//         <li>Australia</li>
+//         <li>Goa</li>
+//       </ol>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(
+//   < MyInfo />,
+//   document.getElementById("root")
+// )
+
+//Styling React Components//
+///******Inline CSS */
+// const divStyle = {
+//     color: 'blue',
+//     backgroundImage: 'url(' + imgUrl + ')',
+//   };
+//   function HelloWorldComponent() {
+//     return <div style={divStyle}>Hello World!</div>;
+
